dee6c0b8eb3a5ccd7bd50f77cdf2bf77cf8d2c0f
Christopher Garcia <christopher.garcia08@gmail.com>
2021-04-30 06:55:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def kind()
+    self.partner = person
+    if person.class != Person
+      begin
+        raise PartnerError
+      rescue PartnerError => error 
+        puts error.message
+      end 
+    else
+      person.partner = self
+    end
+  end
+ 
+  class PartnerError < StandardError
+    def message
+    "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end
+end
+ 
+beyonce = Person.new("Beyonce")
+beyonce.get_married("Jay-Z")
+puts beyonce.name
 end
