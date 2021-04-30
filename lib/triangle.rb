c40fda5fe78b31fe10f3b97a93f8d4cd910aed53
Christopher Garcia <christopher.garcia08@gmail.com>
2021-04-30 07:01:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,6 +3,7 @@ class Triangle
  
   def initialize(side1, side2, side3)
     if side1 >= (side2 + side3) || side2 >= (side1 + side3) || side3 >= (side2 + side1)
+      raise TriangleError
   end
  
   def kind()
