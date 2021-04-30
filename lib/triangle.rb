064e77ecfeb4d9cd75b78052a15c71710a5105ac
Christopher Garcia <christopher.garcia08@gmail.com>
2021-04-30 07:00:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
  
   def initialize(side1, side2, side3)
-    if side1 >= (side2 + side3) ||
+    if side1 >= (side2 + side3) || side2 >= (side1 + side3) || side3 >= (side2 + side1)
   end
  
   def kind()
